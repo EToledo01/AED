8cbbc26e605a79784c38a0e42e74586b6db600fd
EToledo01 <82550121+EToledo01@users.noreply.github.com>
2021-05-05 23:53:32 -0300

    Update EjemploTipos.cpp

--- DD-EjemploTipos/EjemploTipos.cpp
+++ DD-EjemploTipos/EjemploTipos.cpp
@@ -1,1 +1,14 @@
+#include <cassert>
 
+int main()
+{
+
+assert(-4==-1-3); //pruebo int
+assert(2<3); //pruebo unsigned
+assert(1.5==1.5); //pruebo punto flotante
+assert(true and true==true); //pruebo booleano
+assert("a"=="a"); //pruebo char
+assert("hola" != "chau"); //pruebo string
+
+return 0;
+}
